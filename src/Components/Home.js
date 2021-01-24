0396e28f5c2227ad7f0f9503c2b388e93c639890
neil2001 <52434451+neil2001@users.noreply.github.com>
2021-01-24 12:02:34 -0500

    Implemented Navbar

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,7 +6,8 @@ import {
   useHistory,
   useRouteMatch,
 } from "react-router-dom";
-//import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { Navbar, Nav, NavDropdown } from "bootstrap/dist/css/bootstrap.css";
+import BLogo from "./b.png";
 //import { PrivateRoute } from "../App";
 
 import MyGroups from "./MyGroups";
@@ -16,18 +17,50 @@ import Profile from "./Profile";
 const Home = (props) => {
   return (
     <div>
-      <h1>B-POP Forum</h1>
-      <ul>
-        <li>
-          <a href={"/MyGroups"}>My Groups</a>
-        </li>
-        <li>
-          <a href={"/Favorites"}>Favorites</a>
-        </li>
-        <li>
-          <a href={"/Profile"}>Profile</a>
-        </li>
-      </ul>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light ">
+        <a href="/">
+          <img className="logoIcon" src={BLogo} alt="Logo" href="/" />
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="/">
+                Home <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/MyGroups">
+                My Groups
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/Favorites">
+                Favorites
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/Profile">
+                Profile
+              </a>
+            </li>
+          </ul>
+          <div>
+            <button className="rightDivButtons">Log In</button>
+            <button className="rightDivButtons">Sign Up</button>
+          </div>
+        </div>
+      </nav>
       <BrowserRouter>
         <Switch>
           <Route exact path="/MyGroups" component={MyGroups} />
