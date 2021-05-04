97a0f9d1ccf8e998823488e29c1c4e96e5b223d9
neil2001 <52434451+neil2001@users.noreply.github.com>
2021-05-03 23:10:11 -0500

    some fun edits

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -18,19 +18,19 @@ const Menu = ({ open, setOpen }) => {
         <span role="img" aria-label="about us">
           &#x1f481;&#x1f3fb;&#x200d;&#x2642;&#xfe0f;
         </span>
-        About us
+        Favorites
       </a>
       <a href="/">
         <span role="img" aria-label="price">
           &#x1f4b8;
         </span>
-        Pricing
+        My Groups
       </a>
       <a href="/">
         <span role="img" aria-label="contact">
           &#x1f4e9;
         </span>
-        Contact
+        About BPOP
       </a>
     </StyledMenu>
   );
